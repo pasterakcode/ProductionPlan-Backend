dff788db340e63074c78d0a539fc3292a77c0cf5
pasterakcode <78322363+pasterakcode@users.noreply.github.com>
2021-12-18 23:09:57 +0100

    Update users.js

--- backend/router/users.js
+++ backend/router/users.js
@@ -2,13 +2,9 @@ const express = require('express');
 const router = express.Router();
 const userActions = require('../actions/userActions');
 
-// pobranie wszystkich użytkowników 
 router.get('/users', userActions.getUsers);
-// pobieranie jednego użytownika / logowanie
 router.get('/users/login', userActions.loginUser);
-// dodawanie nowego użytkownika
 router.post('/users', userActions.saveUser);
-// kasowanie użytkownika
 router.delete('/users/:id', userActions.deleteUser)
 
 
